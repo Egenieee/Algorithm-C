b6bb1012879035cc43d55e71be406743175bd045
Jinhee Lee <81270604+Egenieee@users.noreply.github.com>
2024-04-15 17:27:53 +0900

    [Silver III] Title: 1, 2, 3 더하기, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

--- 백준/Silver/9095. 1， 2， 3 더하기/1， 2， 3 더하기.cc
+++ 백준/Silver/9095. 1， 2， 3 더하기/1， 2， 3 더하기.cc
@@ -14,16 +14,16 @@ int memo[11];
 int main() {
     cin >> inputT;
 
+    memo[1] = 1;
+    memo[2] = 2;
+    memo[3] = 4;
+
+    for (int i = 4; i < 11; i++) {
+        memo[i] = memo[i - 1] + memo[i - 2] + memo[i - 3];
+    }
+
     for (int t = 0; t < inputT; t++) {
         cin >> inputN;
-
-        memo[1] = 1;
-        memo[2] = 2;
-        memo[3] = 4;
-
-        for (int i = 4; i <= inputN; i++) {
-            memo[i] = memo[i - 1] + memo[i - 2] + memo[i - 3];
-        }
 
         cout << memo[inputN] << "\n";
     }
