71b34c8a5e70af038e3bd0de8dbc11952e377d8a
Jinhee Lee <81270604+Egenieee@users.noreply.github.com>
2024-05-28 11:02:50 +0900

    [Silver III] Title: N과 M (4), Time: 4 ms, Memory: 2020 KB -BaekjoonHub

--- 백준/Silver/15652. N과 M （4）/N과 M （4）.cc
+++ 백준/Silver/15652. N과 M （4）/N과 M （4）.cc
@@ -1,33 +1,36 @@
 #include <iostream>
+#include <vector>
+
 using namespace std;
 
-int n, m;
-int path[8];
+// 1부터 N까지 자연수 중에서 M개를 고른 수열
+// 같은 수를 여러 번 골라도 된다.
+// 고른 수열은 비내림차순이어야 한다.
+// 길이가 K인 수열 A가 A1 ≤ A2 ≤ ... ≤ AK-1 ≤ AK를 만족하면, 비내림차순이라고 한다.
 
-void dfs(int now, int next){
+int inputN, inputM;
+vector<int> numbers;
 
-    if (now == m) {
-        for (int i = 0; i < m; i++){
-            cout << path[i] << " ";
+void dfs(int curLevel, int prev) {
+    if (curLevel == inputM) {
+        for (int number : numbers) {
+            cout << number << " ";
         }
         cout << "\n";
         return;
     }
 
-    
-
-    for (int i = next; i <= n; i++){
-        path[now] = i;
-        dfs(now + 1, i);
-        path[now] = 0;
+    for (int i = prev; i <= inputN; i++) {
+        numbers.push_back(i);
+        dfs(curLevel + 1, i);
+        numbers.pop_back();
     }
-
 }
 
-int main(){
+int main() {
+    cin >> inputN >> inputM;
 
-    cin >> n >> m;
-    
     dfs(0, 1);
 
+    return 0;
 }
